ecfbe20479df16ae77e117123fbf6e3392fff32b
Red Hunter <thredmoonlonewolf@gmail.com>
2025-01-31 10:04:27 +0000

    oragnize files

---
+++ Rust/balance_account/target/debug/deps/balance_account-2e56839c08c8405a.d
@@ -0,0 +1,8 @@
+/home/th-redhunter/10-Projects/12-Side/12.0-Quicks/balance_account/target/debug/deps/libbalance_account-2e56839c08c8405a.rmeta: src/lib.rs src/debug/mod.rs
+
+/home/th-redhunter/10-Projects/12-Side/12.0-Quicks/balance_account/target/debug/deps/libbalance_account-2e56839c08c8405a.rlib: src/lib.rs src/debug/mod.rs
+
+/home/th-redhunter/10-Projects/12-Side/12.0-Quicks/balance_account/target/debug/deps/balance_account-2e56839c08c8405a.d: src/lib.rs src/debug/mod.rs
+
+src/lib.rs:
+src/debug/mod.rs:
---
+++ Rust/balance_account/target/debug/deps/balance_account-e6b463bf725c4226.d
@@ -0,0 +1,5 @@
+/home/th-redhunter/10-Projects/12-Side/12.0-Quicks/balance_account/target/debug/deps/balance_account-e6b463bf725c4226: src/main.rs
+
+/home/th-redhunter/10-Projects/12-Side/12.0-Quicks/balance_account/target/debug/deps/balance_account-e6b463bf725c4226.d: src/main.rs
+
+src/main.rs:
