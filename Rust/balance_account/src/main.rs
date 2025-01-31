d71eab4f2b058f7bb8e50b62e02ad35cdbec6a98
Red Hunter <thredmoonlonewolf@gmail.com>
2025-01-31 18:20:11 +0000

    same the previous

--- Rust/balance_account/src/main.rs
+++ Rust/balance_account/src/main.rs
@@ -1,18 +1,18 @@
 extern crate balance_account;
 
-use std::io;
 use balance_account::debug;
+use std::io;
 
 struct Account {
     name: String,
     password: String,
-    balance: f32
+    balance: f32,
 }
 
-struct Login (String, String);
+struct Login(String, String);
 
 impl Login {
-    fn default() -> (String, String){
+    fn default() -> (String, String) {
         ("".to_string(), "".to_string())
     }
 }
@@ -48,10 +48,10 @@ fn main() {
         let mut input: String = String::new();
         debug::head("options".to_string());
         println!("0. inc\n1. dec\n2. show\n3. exit");
-        io::stdin().read_line(&mut input)
+        io::stdin()
+            .read_line(&mut input)
             .expect("Failed to read the input: ");
-        let input: i32 = input.trim().parse()
-            .expect("enter a number: ");
+        let input: i32 = input.trim().parse().expect("enter a number: ");
         match input {
             0 => account.inc(get_amount()),
             1 => account.dec(get_amount()),
@@ -66,14 +66,15 @@ fn create_account(name_i: String, password_i: String) -> Account {
     Account {
         name: name_i,
         password: password_i,
-        balance: 0.0
+        balance: 0.0,
     }
 }
 
 fn get_amount() -> f32 {
     let mut input: String = String::new();
     debug::input("Enter an amount: ".to_string());
-    io::stdin().read_line(&mut input)
+    io::stdin()
+        .read_line(&mut input)
         .expect("Failed to read the input: ");
     input.trim().parse().expect("wrong input")
 }
@@ -83,10 +84,12 @@ fn get_login(header: String) -> Login {
 
     debug::head(header);
     debug::input("Enter your name:".to_string());
-    io::stdin().read_line(&mut input.0)
+    io::stdin()
+        .read_line(&mut input.0)
         .expect("Failed to read the input: ");
     debug::input("Enter your password: ".to_string());
-    io::stdin().read_line(&mut input.1)
+    io::stdin()
+        .read_line(&mut input.1)
         .expect("Failed to read the input: ");
 
     Login(input.0, input.1)
